b7c31d7b132d76b04ecda9a21809c9210a1b39b4
divadssdavid <gomezdavidc@gmail.com>
2013-07-11 17:36:36 +0000

    edited web.js again 11

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var message = fs.readFileSync('index.html', 'utf-8');
+    var message = fs.readFileSync('bitstarter.html', 'utf-8');
     response.send(message);
 });
 
