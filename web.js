25b664ed4bfff4ebb87ca3aaf9dbd01c1a5b0e5c
divadssdavid <gomezdavidc@gmail.com>
2013-07-11 16:47:55 +0000

    edited web.js again 6

--- web.js
+++ web.js
@@ -2,11 +2,18 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html', function(err, data) {
-        data.toString('utf-8');
-    } ););
+    var fs = require('fs');
+    var buffer = new buffer();
+    response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
 });
+
+// app.get('/', function(request, response) {
+//     response.send(fs.readFileSync('index.html', function(err, data) {
+//         data.toString('utf-8');
+//     }););
+// });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
